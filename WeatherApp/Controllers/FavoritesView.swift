5a45566b81d1853a350d4866cba6ceae24533fcd
levi davis <levidavis@pursuit.org>
2019-10-10 17:13:05 -0400

    Adds favorites view and favorites view controller files

---
+++ WeatherApp/Controllers/FavoritesView.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesView.swift
+//  WeatherApp
+//
+//  Created by Levi Davis on 10/10/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
